bcb1f011637b3f0670155fe8bf1ce3015ef04a00
timetraveler <timetraveler930@gmail.com>
2023-06-29 21:52:21 +0900

    add default mavlink option

--- modules/settings/mavlink/mavlink.gd
+++ modules/settings/mavlink/mavlink.gd
@@ -1,5 +1,9 @@
 extends Control
 
+@export var console_connect:OptionButton
+@export var console_disconnect:OptionButton
+@export var joystick_control:OptionButton
+@export var virtual_joystick:OptionButton
 @export var system_id:LineEdit
 @export var component_id:LineEdit
 @export var udp_portforward:OptionButton
@@ -7,9 +11,42 @@ extends Control
 @export var autoconnect:LineEdit
 @export var autoconnect_list:ItemList
 
-func _ready():
-	# TODO implement initial values
-	pass
+func reset_to_default():
+	console_connect.select(0)
+	console_connect.item_selected.emit(0)
+	
+	console_disconnect.select(0)
+	console_disconnect.item_selected.emit(0)
+	
+	joystick_control.select(1)
+	joystick_control.item_selected.emit(1)
+	
+	virtual_joystick.select(1)
+	virtual_joystick.item_selected.emit(1)
+	
+	system_id.text = str(MavlinkSettings.DEFAULT_SYS_ID)
+	system_id.text_submitted.emit(system_id.text)
+	
+	component_id.text = str(MavlinkSettings.DEFAULT_COMP_ID)
+	component_id.text_submitted.emit(component_id.text)
+	
+	udp_portforward.select(0)
+	udp_portforward_port.text = str(MavlinkSettings.DEFAULT_UDP_PORTFORWARD_PORT)
+	MavlinkSettings.udp_portforward_changed.emit(false, MavlinkSettings.DEFAULT_UDP_PORTFORWARD_PORT)
+	
+	# remove all items in list
+	while autoconnect_list.item_count > 0:
+		autoconnect_list.select(0)
+		_on_autoconnect_remove_pressed()
+	
+	# Add default item 
+	for key in MavlinkSettings.DEFAULT_AUTOCONNECT_LIST:
+		var autoConnect:MavlinkSettings.AutoConnect = MavlinkSettings.AutoConnect.new()
+		autoConnect.index = autoconnect_list.add_item(key)
+		autoConnect.enabled = MavlinkSettings.DEFAULT_AUTOCONNECT_LIST[key]
+		MavlinkSettings.AutoConnectList.merge({key:autoConnect})
+		if !autoConnect.enabled:
+			autoconnect_list.set_item_custom_fg_color(autoConnect.index, Color(1.0, 1.0, 1.0, 0.2))
 
 func _on_console_connected_item_selected(index):
 	match index:
@@ -55,67 +92,82 @@ func _on_system_id_text_submitted(new_text:String):
 	if new_text.is_valid_int():
 		MavlinkSettings.system_id_changed.emit(new_text.to_int())
 	else:
-		# reset LineEdit
+		Notification.notify(" Unvalid system id ", Notification.NOTICE_TYPE.ALERT)
 		system_id.clear()
-	pass # Replace with function body.
 
 func _on_component_id_text_submitted(new_text:String):
-	if new_text.is_valid_int():
-		MavlinkSettings.component_id_changed.emit(new_text.to_int())
-	else:
-		# reset LineEdit
+	if !new_text.is_valid_int():
+		Notification.notify(" Unvalid comp id ", Notification.NOTICE_TYPE.ALERT)
 		component_id.clear()
-	pass 
+		return
+		
+	MavlinkSettings.component_id_changed.emit(new_text.to_int())
 
 func _on_udp_portforward_item_selected(index):
 	if udp_portforward_port.text.is_valid_int() == false:
+		Notification.notify(" Unvalid port ", Notification.NOTICE_TYPE.ALERT)
 		udp_portforward_port.clear()
 		return
+		
 	match index:
 		0:
 			MavlinkSettings.udp_portforward_changed.emit(false, udp_portforward_port.text.to_int())
 		1:
 			MavlinkSettings.udp_portforward_changed.emit(true, udp_portforward_port.text.to_int())
-	pass
 
 func _on_udp_portforward_port_text_submitted(new_text:String):
-	if new_text.is_valid_int():
-		match udp_portforward.get_selected_id():
-			0:
-				MavlinkSettings.udp_portforward_changed.emit(false, new_text.to_int())
-			1:
-				MavlinkSettings.udp_portforward_changed.emit(true, new_text.to_int())
-				pass
-	else:
+	if !new_text.is_valid_int():
+		Notification.notify(" Unvalid port ", Notification.NOTICE_TYPE.ALERT)
 		udp_portforward_port.clear()
-	pass
+		return 
+		
+	match udp_portforward.get_selected_id():
+		0:
+			MavlinkSettings.udp_portforward_changed.emit(false, new_text.to_int())
+		1:
+			MavlinkSettings.udp_portforward_changed.emit(true, new_text.to_int())
+			pass
 
 func _on_autoconnect_add_pressed():
-	MavlinkSettings.auto_connect_added.emit(autoconnect.text)
-	pass
+	if autoconnect.text.is_empty():
+		Notification.notify(" Empty Connection Address ", Notification.NOTICE_TYPE.ALERT)
+		return 
+		
+	if MavlinkSettings.AutoConnectList.has(autoconnect.text):
+		Notification.notify(" Connection Address Already Exist ", Notification.NOTICE_TYPE.WARNING)
+		return 
+	# TODO Check about string by using REGEX?
+	
+	var autoConnect:MavlinkSettings.AutoConnect = MavlinkSettings.AutoConnect.new()
+	autoConnect.index = autoconnect_list.add_item(autoconnect.text)
+	autoConnect.enabled = true
+	MavlinkSettings.AutoConnectList.merge({autoconnect.text:autoConnect})
 
 func _on_autoconnect_remove_pressed():
-	if autoconnect_list.is_anything_selected():
-		var selected_items:PackedInt32Array = autoconnect_list.get_selected_items()
-		for selected_item in selected_items:
-			MavlinkSettings.auto_connect_removed.emit(autoconnect_list.get_item_text(selected_item))
-	pass
+	if !autoconnect_list.is_anything_selected():
+		Notification.notify(" Select AutoConnect item ", Notification.NOTICE_TYPE.WARNING)
+		return
+	
+	var selected_item:int = autoconnect_list.get_selected_items()[0]
+	MavlinkSettings.AutoConnectList.erase(autoconnect_list.get_item_text(selected_item))
+	autoconnect_list.remove_item(selected_item)
 
 func _on_autoconnect_enable_pressed():
-	if autoconnect_list.is_anything_selected():
-		var selected_items:PackedInt32Array = autoconnect_list.get_selected_items()
-		for selected_item in selected_items:
-			MavlinkSettings.auto_connect_enabled.emit(autoconnect_list.get_item_text(selected_item))
-			autoconnect_list.set_item_custom_fg_color(selected_item, Color(1.0, 1.0, 1.0, 1.0))
-			autoconnect_list.set_item_tooltip(selected_item, "enabled")
-	pass
-
+	if !autoconnect_list.is_anything_selected():
+		Notification.notify(" Select AutoConnect item ", Notification.NOTICE_TYPE.WARNING)
+		return
+	
+	var selected_item:int = autoconnect_list.get_selected_items()[0]
+	MavlinkSettings.AutoConnectList[autoconnect_list.get_item_text(selected_item)].enabled = true
+	autoconnect_list.set_item_custom_fg_color(selected_item, Color(1.0, 1.0, 1.0, 1.0))
+	autoconnect_list.set_item_tooltip(selected_item, "enabled")
+			
 func _on_autoconnect_disable_pressed():
-	if autoconnect_list.is_anything_selected():
-		var selected_items:PackedInt32Array = autoconnect_list.get_selected_items()
-		for selected_item in selected_items:
-			MavlinkSettings.auto_connect_disabled.emit(autoconnect_list.get_item_text(selected_item))
-			autoconnect_list.set_item_custom_fg_color(selected_item, Color(1.0, 1.0, 1.0, 0.2))
-			autoconnect_list.set_item_tooltip(selected_item, "disabled")
-	pass
-
+	if !autoconnect_list.is_anything_selected():
+		Notification.notify(" Select AutoConnect item ", Notification.NOTICE_TYPE.WARNING)
+		return
+	
+	var selected_item:int = autoconnect_list.get_selected_items()[0]
+	MavlinkSettings.AutoConnectList[autoconnect_list.get_item_text(selected_item)].enabled = false
+	autoconnect_list.set_item_custom_fg_color(selected_item, Color(1.0, 1.0, 1.0, 0.2))
+	autoconnect_list.set_item_tooltip(selected_item, "disabled")
--- modules/settings/mavlink/mavlink_settings.gd
+++ modules/settings/mavlink/mavlink_settings.gd
@@ -13,25 +13,32 @@ signal joystick_enabled(enable:bool)
 signal virutal_joystick_enabled(enable:bool)
 
 # mavsdk sysid settings
+const DEFAULT_SYS_ID:int = 245
 var system_id:int
 signal system_id_changed(sys_id:int)
 
 # mavsdk compid settings
+const DEFAULT_COMP_ID:int = 190
 var component_id:int
 signal component_id_changed(component_id:int)
 
 # portforward settings
+const DEFAULT_UDP_PORTFORWARD_PORT = 14446
 var udp_portforward_enabled:bool
 var udp_portforward_port:int
 signal udp_portforward_changed(enable:bool, port:int)
 
 # mavlink auto connection settings
-var auto_connect_list:PackedStringArray
-var auto_connect_list_enabled:PackedByteArray
-signal auto_connect_added(connection:String)
-signal auto_connect_removed(connection:String)
-signal auto_connect_enabled(connection:String)
-signal auto_connect_disabled(connection:String)
+class AutoConnect:
+	var index:int # ItemList index
+	var enabled:bool
+	
+var AutoConnectList:Dictionary # contains [address(String), AutoConnect]
+
+const DEFAULT_AUTOCONNECT_LIST:Dictionary = {
+	"udp://:14550" : true,		 	# index = 0, enabled = true
+	"serial://COM3:57600" : false 	# index = 1, enabled = false
+}
 
 func _ready():
 	# TODO implement save-load
