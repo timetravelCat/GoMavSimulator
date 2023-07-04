80aaec39f5b21387d7efb7aea47b0cd42e840218
timetraveler <timetraveler930@gmail.com>
2023-07-05 00:41:31 +0900

    merge district setting to general setting

--- modules/settings/general/general_settings.gd
+++ modules/settings/general/general_settings.gd
@@ -1,15 +1,35 @@
 extends Node
 
 var selected_world:int = 0
-var worlds:Array
+var world_volumetric = preload("res://modules/world/environment/Volumetric.tscn")
+
+var district_mumbai:PackedScene = preload("res://assets/district/cartoon/mumbai.tscn")
+var district_mini:PackedScene = preload("res://assets/district/cartoon/mini.tscn")
 
 func _enter_tree():
-	# Register worldEnvironments	
-	worlds.append(preload("res://modules/world/environment/Volumetric.tscn"))
-
+	# TODO implement save-load
+	# TODO select by start settings
+	pass
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	add_child(worlds[selected_world].instantiate())
+	# Regieter world
+	var world:Node3D = world_volumetric.instantiate()
+	world.name = "world"
+	add_child(world)
+	
+	# Register district
+	var district:Node3D = district_mumbai.instantiate()
+	district.name = "district"
+	recursive_create_collision_body(district)
+	add_child(district)
 
 func get_current_world() -> WorldEnvironment:
-	return get_child(0).get_node("WorldEnvironment")
+	return find_child("world").get_node("WorldEnvironment")
+
+func recursive_create_collision_body(district_child:Node):
+	for child in district_child.get_children():
+		recursive_create_collision_body(child)
+		var meshInstance = child as MeshInstance3D
+		if meshInstance: # in case of meshinstance, 
+			meshInstance.create_trimesh_collision()
