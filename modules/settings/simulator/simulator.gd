b7a765818a207a0e1878d4be1b387de912d54131
timetraveler <timetraveler930@gmail.com>
2023-07-06 01:24:52 +0900

    revert simulator settings

--- modules/settings/simulator/simulator.gd
+++ modules/settings/simulator/simulator.gd
@@ -18,6 +18,7 @@ extends Control
 # sensor settings
 @export var SensorType:OptionButton
 @export var SensorName:LineEdit
+@export var FrameID:LineEdit
 @export var PublishRate:LineEdit
 @export var LocationX:LineEdit
 @export var LocationY:LineEdit
@@ -48,6 +49,7 @@ func reset_to_default():
 	
 	SensorType.select(0)
 	SensorName.clear()
+	FrameID.text = "/map"
 	PublishRate.clear()
 	LocationX.text = "0.0"
 	LocationY.text = "0.0"
@@ -83,21 +85,21 @@ func _on_vehicle_add_pressed():
 		Notification.notify(" Exist Veihcle Name ", Notification.NOTICE_TYPE.ALERT)
 		return
 	
-	var NewVehicle = SimulatorSettings.VehicleSetting.new()
-	NewVehicle.enable = true
-	NewVehicle.name = VehicleName.text
-	NewVehicle.type = VehicleType.selected
-	NewVehicle.scale = VehicleScale.value / SimulatorSettings.VEHICLE_DEFAULT_SCALE
-	NewVehicle.pose_source = PoseSource.selected
-	NewVehicle.domain_id = DomainID.text.to_int()
-#	# advanced setting
-	NewVehicle.mavlink_pose_source = SimulatorSettings.MAVLINK_POSE_SOURCE_DEFAULT
-	NewVehicle.sys_id = SimulatorSettings.VEHILE_SYSID_DEFAULT
-	NewVehicle.ros2_pose_source = SimulatorSettings.ROS2_POSE_TOPIC_DEFAULT
-	
-	# Register VehicleList
-	NewVehicle.item_id = VehicleList.add_item(NewVehicle.name)
-	SimulatorSettings.VehicleList.merge({NewVehicle.name: NewVehicle})
+#	var NewVehicle = SimulatorSettings.VehicleSetting.new()
+#	NewVehicle.enable = true
+#	NewVehicle.name = VehicleName.text
+#	NewVehicle.type = VehicleType.selected
+#	NewVehicle.scale = VehicleScale.value / SimulatorSettings.VEHICLE_DEFAULT_SCALE
+#	NewVehicle.pose_source = PoseSource.selected
+#	NewVehicle.domain_id = DomainID.text.to_int()
+##	# advanced setting
+#	NewVehicle.mavlink_pose_source = SimulatorSettings.MAVLINK_POSE_SOURCE_DEFAULT
+#	NewVehicle.sys_id = SimulatorSettings.VEHILE_SYSID_DEFAULT
+#	NewVehicle.ros2_pose_source = SimulatorSettings.ROS2_POSE_TOPIC_DEFAULT
+#
+#	# Register VehicleList
+#	NewVehicle.item_id = VehicleList.add_item(NewVehicle.name)
+#	SimulatorSettings.VehicleList.merge({NewVehicle.name: NewVehicle})
 
 func _on_vehicle_delete_pressed():
 	if !VehicleList.is_anything_selected():
@@ -116,43 +118,47 @@ func _on_vehicle_disable_pressed():
 	
 	var item:int = VehicleList.get_selected_items()[0]
 	var key:String = VehicleList.get_item_text(item)
-	if SimulatorSettings.VehicleList.has(key):
-		var vehicle := SimulatorSettings.VehicleList[key] as SimulatorSettings.VehicleSetting
-		vehicle.enable = !vehicle.enable
-		
-		# set color
-		if !vehicle.enable:
-			VehicleList.set_item_custom_fg_color(item, Color(1.0, 1.0, 1.0, 0.2))	
-			VehicleDisable.text = "ENABLE"
-		else :
-			VehicleList.set_item_custom_fg_color(item, Color(1.0, 1.0, 1.0, 1.0)) # Enabled color
-			VehicleDisable.text = "DISABLE"
+#	if SimulatorSettings.VehicleList.has(key):
+#		var vehicle := SimulatorSettings.VehicleList[key] as SimulatorSettings.VehicleSetting
+#		vehicle.enable = !vehicle.enable
+#
+#		# set color
+#		if !vehicle.enable:
+#			VehicleList.set_item_custom_fg_color(item, Color(1.0, 1.0, 1.0, 0.2))	
+#			VehicleDisable.text = "ENABLE"
+#		else :
+#			VehicleList.set_item_custom_fg_color(item, Color(1.0, 1.0, 1.0, 1.0)) # Enabled color
+#			VehicleDisable.text = "DISABLE"
 	
 func _on_vehicle_list_item_selected(index):
 	var key:String = VehicleList.get_item_text(index)
-	if SimulatorSettings.VehicleList.has(key):
-		var vehicle := SimulatorSettings.VehicleList[key] as SimulatorSettings.VehicleSetting
-		
-		VehicleType.select(vehicle.type)
-		VehicleName.text = vehicle.name
-		DomainID.text = str(vehicle.domain_id)
-		PoseSource.select(vehicle.pose_source)
-		VehicleScale.set_value_no_signal(vehicle.scale * SimulatorSettings.VEHICLE_DEFAULT_SCALE)
-		VehicleDisable.text = "DISABLE" if vehicle.enable else "ENABLE"
-		MavlinkPoseSource.select(vehicle.mavlink_pose_source)
-		MavlinkSysId.text = str(vehicle.sys_id)
-		Ros2PoseSource.text = vehicle.ros2_pose_source
-		
-		# Set sensor item list
-		SensorList.clear()
-		for sensor_name in vehicle.sensors.keys():
-			SensorList.add_item(sensor_name)
+#	if SimulatorSettings.VehicleList.has(key):
+#		var vehicle := SimulatorSettings.VehicleList[key] as SimulatorSettings.VehicleSetting
+#
+#		VehicleType.select(vehicle.type)
+#		VehicleName.text = vehicle.name
+#		DomainID.text = str(vehicle.domain_id)
+#		PoseSource.select(vehicle.pose_source)
+#		VehicleScale.set_value_no_signal(vehicle.scale * SimulatorSettings.VEHICLE_DEFAULT_SCALE)
+#		VehicleDisable.text = "DISABLE" if vehicle.enable else "ENABLE"
+#		MavlinkPoseSource.select(vehicle.mavlink_pose_source)
+#		MavlinkSysId.text = str(vehicle.sys_id)
+#		Ros2PoseSource.text = vehicle.ros2_pose_source
+#
+#		# Set sensor item list
+#		SensorList.clear()
+#		for sensor_name in vehicle.sensors.keys():
+#			SensorList.add_item(sensor_name)
 
 func _on_sensor_add_pressed():
 	if SensorName.text.is_empty():
 		Notification.notify(" Empty Sensor Name ", Notification.NOTICE_TYPE.ALERT)
 		return
-		
+	
+	if FrameID.text.is_empty():
+		Notification.notify(" Empty Frame Name ", Notification.NOTICE_TYPE.ALERT)
+		return
+	
 	if !PublishRate.text.is_valid_float():
 		Notification.notify(" Invalid Publish Rate ", Notification.NOTICE_TYPE.ALERT)
 		return
@@ -165,105 +171,109 @@ func _on_sensor_add_pressed():
 		Notification.notify(" Invalid Rotation ", Notification.NOTICE_TYPE.ALERT)
 		return
 	
-	var vehicle:SimulatorSettings.VehicleSetting = get_selected_vehicle()
-	if vehicle == null:
-		return
-		
-	if vehicle.sensors.has(SensorName.text):
-		Notification.notify(" Exist Sensor Name ", Notification.NOTICE_TYPE.ALERT)
-		return
-	
-	var NewSensor = SimulatorSettings.Sensor.new()
-	NewSensor.enable = true
-	NewSensor.type = SensorType.selected
-	NewSensor.name = SensorName.text
-	NewSensor.hz = PublishRate.text.to_float()
-	# Stored as EUS frame
-	NewSensor.location = ENU2EUS.enu_to_eus_v(Vector3(LocationX.text.to_float(), LocationY.text.to_float(), LocationZ.text.to_float())) 
-	NewSensor.rotation = ENU2EUS.enu_to_eus_b(Basis.from_euler(Vector3(deg_to_rad(RotationX.text.to_float()), deg_to_rad(RotationY.text.to_float()), deg_to_rad(RotationZ.text.to_float())), EULER_ORDER_ZYX))
-	
-	# Advanced configuration
-	match NewSensor.type: 	# NewSensor.resoultion =  Vector2i(CameraWitdh.text.to_int(), CameraHeight.text.to_int())
-		SimulatorSettings.SENSOR_TYPE.RGB_CAMERA:
-			NewSensor.resoultion = SimulatorSettings.RGB_CAMERA_DEFAULT_RESOLUTION
-		SimulatorSettings.SENSOR_TYPE.DEPTH_CAMERA:
-			NewSensor.resoultion = SimulatorSettings.DEPTH_CAMERA_DEFUALT_RESOLUTION
-			
-	NewSensor.vertical_fov = SimulatorSettings.LIDAR_DEFAULT_VERTICAL_FOV # deg_to_rad(LidarVerticalFov.text.to_float())
-	NewSensor.vertical_resolution = SimulatorSettings.LIDAR_DEFAULT_VERTICAL_RESOLUTION # deg_to_rad(LidarHeigth.text.to_float())
-	NewSensor.horizontal_resolution = SimulatorSettings.LIDAR_DEFAULT_HORIZONTAL_RESOLUTION # deg_to_rad(LidarWidth.text.to_float())
-	
-	# Register to SensorList
-	NewSensor.item_id = SensorList.add_item(NewSensor.name)
-	vehicle.sensors.merge({NewSensor.name: NewSensor})
+#	var vehicle:SimulatorSettings.VehicleSetting = get_selected_vehicle()
+#	if vehicle == null:
+#		return
+#
+#	if vehicle.sensors.has(SensorName.text):
+#		Notification.notify(" Exist Sensor Name ", Notification.NOTICE_TYPE.ALERT)
+#		return
+#
+#	var NewSensor = SimulatorSettings.Sensor.new()
+#	NewSensor.enable = true
+#	NewSensor.type = SensorType.selected
+#	NewSensor.name = SensorName.text
+#	NewSensor.hz = PublishRate.text.to_float()
+#	# Stored as EUS frame
+#	NewSensor.location = ENU2EUS.enu_to_eus_v(Vector3(LocationX.text.to_float(), LocationY.text.to_float(), LocationZ.text.to_float())) 
+#	NewSensor.rotation = ENU2EUS.enu_to_eus_b(Basis.from_euler(Vector3(deg_to_rad(RotationX.text.to_float()), deg_to_rad(RotationY.text.to_float()), deg_to_rad(RotationZ.text.to_float())), EULER_ORDER_ZYX))
+#
+#	# Advanced configuration
+#	match NewSensor.type: 	# NewSensor.resoultion =  Vector2i(CameraWitdh.text.to_int(), CameraHeight.text.to_int())
+#		SimulatorSettings.SENSOR_TYPE.RGB_CAMERA:
+#			NewSensor.resoultion = SimulatorSettings.RGB_CAMERA_DEFAULT_RESOLUTION
+#		SimulatorSettings.SENSOR_TYPE.DEPTH_CAMERA:
+#			NewSensor.resoultion = SimulatorSettings.DEPTH_CAMERA_DEFUALT_RESOLUTION
+#
+#	NewSensor.vertical_fov = SimulatorSettings.LIDAR_DEFAULT_VERTICAL_FOV # deg_to_rad(LidarVerticalFov.text.to_float())
+#	NewSensor.vertical_resolution = SimulatorSettings.LIDAR_DEFAULT_VERTICAL_RESOLUTION # deg_to_rad(LidarHeigth.text.to_float())
+#	NewSensor.horizontal_resolution = SimulatorSettings.LIDAR_DEFAULT_HORIZONTAL_RESOLUTION # deg_to_rad(LidarWidth.text.to_float())
+#
+#	# Register to SensorList
+#	NewSensor.item_id = SensorList.add_item(NewSensor.name)
+#	vehicle.sensors.merge({NewSensor.name: NewSensor})
 
 func _on_sensor_delete_pressed():
-	var vehicle:SimulatorSettings.VehicleSetting = get_selected_vehicle()
-	if vehicle == null:
-		return
-	
-	var item:int = SensorList.get_selected_items()[0]
-	var key:String = SensorList.get_item_text(item)
-	vehicle.sensors.erase(key)
-	SensorList.remove_item(item)
+#	var vehicle:SimulatorSettings.VehicleSetting = get_selected_vehicle()
+#	if vehicle == null:
+#		return
+#
+#	var item:int = SensorList.get_selected_items()[0]
+#	var key:String = SensorList.get_item_text(item)
+#	vehicle.sensors.erase(key)
+#	SensorList.remove_item(item)
+	pass
 
 func _on_sensor_disable_pressed():
-	var vehicle:SimulatorSettings.VehicleSetting = get_selected_vehicle()
-	if vehicle == null:
-		return
-		
-	var item:int = SensorList.get_selected_items()[0]
-	var key:String = SensorList.get_item_text(item)
-	if vehicle.sensors.has(key):
-		var sensor := vehicle.sensors[key] as SimulatorSettings.Sensor
-		sensor.enable = !sensor.enable
-		
-		if !sensor.enable:
-			SensorList.set_item_custom_fg_color(item, Color(1.0, 1.0, 1.0, 0.2))
-			SensorDisable.text = "ENABLE"
-		else :
-			SensorList.set_item_custom_fg_color(item, Color(1.0, 1.0, 1.0, 1.0))
-			SensorDisable.text = "DISABLE"
+#	var vehicle:SimulatorSettings.VehicleSetting = get_selected_vehicle()
+#	if vehicle == null:
+#		return
+#
+#	var item:int = SensorList.get_selected_items()[0]
+#	var key:String = SensorList.get_item_text(item)
+#	if vehicle.sensors.has(key):
+#		var sensor := vehicle.sensors[key] as SimulatorSettings.Sensor
+#		sensor.enable = !sensor.enable
+#
+#		if !sensor.enable:
+#			SensorList.set_item_custom_fg_color(item, Color(1.0, 1.0, 1.0, 0.2))
+#			SensorDisable.text = "ENABLE"
+#		else :
+#			SensorList.set_item_custom_fg_color(item, Color(1.0, 1.0, 1.0, 1.0))
+#			SensorDisable.text = "DISABLE"
+	pass
 	
 func _on_sensor_list_item_selected(index):
-	var vehicle:SimulatorSettings.VehicleSetting = get_selected_vehicle()
-	if vehicle == null:
-		return
-	
-	var key:String = SensorList.get_item_text(index)
-	var sensor := vehicle.sensors[key] as SimulatorSettings.Sensor
-	
-	SensorType.select(sensor.type)
-	SensorName.text = sensor.name
-	PublishRate.text = str(sensor.hz)
-	var sensor_position:Vector3 = ENU2EUS.eus_to_enu_v(sensor.location)
-	var sensor_rotation:Basis = ENU2EUS.eus_to_enu_b(sensor.rotation)
-	LocationX.text = str(sensor_position.x)
-	LocationY.text = str(sensor_position.y)
-	LocationZ.text = str(sensor_position.z)
-	var euler:Vector3 = sensor_rotation.get_euler(EULER_ORDER_ZYX)
-	RotationX.text = str(rad_to_deg(euler.x))
-	RotationY.text = str(rad_to_deg(euler.y))
-	RotationZ.text = str(rad_to_deg(euler.z))
-	
-	SensorDisable.text = "DISABLE" if sensor.enable else "ENABLE"
-	CameraWitdh.text = str(sensor.resoultion.x)
-	CameraHeight.text = str(sensor.resoultion.y)
-	LidarVerticalFov.text = str(rad_to_deg(sensor.vertical_fov))
-	LidarWidth.text = str(rad_to_deg(sensor.horizontal_resolution))
-	LidarHeigth.text = str(rad_to_deg(sensor.vertical_resolution))
-	RangeDistance.text = str(sensor.range_distance)
+#	var vehicle:SimulatorSettings.VehicleSetting = get_selected_vehicle()
+#	if vehicle == null:
+#		return
+#
+#	var key:String = SensorList.get_item_text(index)
+#	var sensor := vehicle.sensors[key] as SimulatorSettings.Sensor
+#
+#	SensorType.select(sensor.type)
+#	SensorName.text = sensor.name
+#  	FrameID.text = sensor.frame_id
+#	PublishRate.text = str(sensor.hz)
+#	var sensor_position:Vector3 = ENU2EUS.eus_to_enu_v(sensor.location)
+#	var sensor_rotation:Basis = ENU2EUS.eus_to_enu_b(sensor.rotation)
+#	LocationX.text = str(sensor_position.x)
+#	LocationY.text = str(sensor_position.y)
+#	LocationZ.text = str(sensor_position.z)
+#	var euler:Vector3 = sensor_rotation.get_euler(EULER_ORDER_ZYX)
+#	RotationX.text = str(rad_to_deg(euler.x))
+#	RotationY.text = str(rad_to_deg(euler.y))
+#	RotationZ.text = str(rad_to_deg(euler.z))
+#
+#	SensorDisable.text = "DISABLE" if sensor.enable else "ENABLE"
+#	CameraWitdh.text = str(sensor.resoultion.x)
+#	CameraHeight.text = str(sensor.resoultion.y)
+#	LidarVerticalFov.text = str(rad_to_deg(sensor.vertical_fov))
+#	LidarWidth.text = str(rad_to_deg(sensor.horizontal_resolution))
+#	LidarHeigth.text = str(rad_to_deg(sensor.vertical_resolution))
+#	RangeDistance.text = str(sensor.range_distance)
+	pass
 
-func get_selected_vehicle() -> SimulatorSettings.VehicleSetting:
-	if !VehicleList.is_anything_selected():
-		Notification.notify(" Select Veihcle ", Notification.NOTICE_TYPE.ALERT)
-		return null
-	
-	if VehicleList.get_selected_items().size() > 1:
-		Notification.notify(" Selected Multiple Veihcle ", Notification.NOTICE_TYPE.ALERT)
-		return null
-	
-	return SimulatorSettings.VehicleList.get(VehicleList.get_item_text(VehicleList.get_selected_items()[0]))
+#func get_selected_vehicle() -> SimulatorSettings.VehicleSetting:
+#	if !VehicleList.is_anything_selected():
+#		Notification.notify(" Select Veihcle ", Notification.NOTICE_TYPE.ALERT)
+#		return null
+#
+#	if VehicleList.get_selected_items().size() > 1:
+#		Notification.notify(" Selected Multiple Veihcle ", Notification.NOTICE_TYPE.ALERT)
+#		return null
+#
+#	return SimulatorSettings.VehicleList.get(VehicleList.get_item_text(VehicleList.get_selected_items()[0]))
 	
 func _on_vehicle_advanced_config_pressed():
 	if !VehicleList.is_anything_selected():
--- modules/settings/simulator/simulator_settings.gd
+++ modules/settings/simulator/simulator_settings.gd
@@ -1,55 +1,55 @@
 extends Node
 
-# Vehicle configuritions
-enum VEHICLE_TYPE {MULTICOPTER, ROVER}
-enum POSE_SOURCE {MAVLINK, ROS2, NONE}
-enum MAVLINK_POSE_SOURCE {GROUND_TRUTH, ESTIMATION}
-const VEHICLE_DEFAULT_SCALE:float=10.0
+## Vehicle configuritions
+#enum VEHICLE_TYPE {MULTICOPTER, ROVER}
+#enum POSE_SOURCE {MAVLINK, ROS2, NONE}
+#enum MAVLINK_POSE_SOURCE {GROUND_TRUTH, ESTIMATION}
+#const VEHICLE_DEFAULT_SCALE:float=10.0
+#
+## Default advanced vehicle setting defaults
+#const MAVLINK_POSE_SOURCE_DEFAULT=SimulatorSettings.MAVLINK_POSE_SOURCE.GROUND_TRUTH
+#const VEHILE_SYSID_DEFAULT:int=1
+#const ROS2_POSE_TOPIC_DEFAULT:String="pose"
+#
+## Default advanced sensor settings defaults
+#const RANGE_DEFAULT_DISTANCE:float = 100.0
+#const RGB_CAMERA_DEFAULT_RESOLUTION:Vector2i = Vector2i(640, 360)
+#const DEPTH_CAMERA_DEFUALT_RESOLUTION:Vector2i = Vector2i(360, 180)
+#const LIDAR_DEFAULT_VERTICAL_FOV:float = 0.0
+#const LIDAR_DEFAULT_VERTICAL_RESOLUTION:float = 0.0 
+#const LIDAR_DEFAULT_HORIZONTAL_RESOLUTION:float = deg_to_rad(5.0)
 
-# Default advanced vehicle setting defaults
-const MAVLINK_POSE_SOURCE_DEFAULT=SimulatorSettings.MAVLINK_POSE_SOURCE.GROUND_TRUTH
-const VEHILE_SYSID_DEFAULT:int=1
-const ROS2_POSE_TOPIC_DEFAULT:String="pose"
+#class VehicleSetting:
+#	var item_id:int = 0 # itemlist control id
+#	var enable:bool = true
+#	var name:String = "sim"
+#	var type:VEHICLE_TYPE = VEHICLE_TYPE.MULTICOPTER
+#	var scale:float = 1.0
+#	var pose_source:POSE_SOURCE = POSE_SOURCE.MAVLINK
+#	var domain_id:int = 0
+#	# advanced setting
+#	var mavlink_pose_source:MAVLINK_POSE_SOURCE = MAVLINK_POSE_SOURCE_DEFAULT
+#	var sys_id:int = VEHILE_SYSID_DEFAULT# mavlink system id
+#	var ros2_pose_source:String # name of PoseStamped topic.
+#	var sensors:Dictionary # ["name", Sensor]
+#
+#var VehicleList:Dictionary # Array of dictionary ["name", Vehicle]
 
-# Default advanced sensor settings defaults
-const RANGE_DEFAULT_DISTANCE:float = 100.0
-const RGB_CAMERA_DEFAULT_RESOLUTION:Vector2i = Vector2i(640, 360)
-const DEPTH_CAMERA_DEFUALT_RESOLUTION:Vector2i = Vector2i(360, 180)
-const LIDAR_DEFAULT_VERTICAL_FOV:float = 0.0
-const LIDAR_DEFAULT_VERTICAL_RESOLUTION:float = 0.0 
-const LIDAR_DEFAULT_HORIZONTAL_RESOLUTION:float = deg_to_rad(5.0)
-
-class VehicleSetting:
-	var item_id:int = 0 # itemlist control id
-	var enable:bool = true
-	var name:String = "sim"
-	var type:VEHICLE_TYPE = VEHICLE_TYPE.MULTICOPTER
-	var scale:float = 1.0
-	var pose_source:POSE_SOURCE = POSE_SOURCE.MAVLINK
-	var domain_id:int = 0
-	# advanced setting
-	var mavlink_pose_source:MAVLINK_POSE_SOURCE = MAVLINK_POSE_SOURCE_DEFAULT
-	var sys_id:int = VEHILE_SYSID_DEFAULT# mavlink system id
-	var ros2_pose_source:String # name of PoseStamped topic.
-	var sensors:Dictionary # ["name", Sensor]
-
-var VehicleList:Dictionary # Array of dictionary ["name", Vehicle]
-
-# Sensor configurations
-enum SENSOR_TYPE {RGB_CAMERA, DEPTH_CAMERA, RANGE, LIDAR}
-
-# Sensor dictionary structure
-class Sensor:
-	var item_id:int # itemlist control id
-	var enable:bool
-	var name:String # name of ros2 published topic
-	var type:SENSOR_TYPE
-	var location:Vector3
-	var rotation:Basis
-	var hz:float # publish hz
-	# advanced setting
-	var resoultion:Vector2i # CAMERA TYPE RESOLUTION
-	var vertical_fov:float # LIDAR in radian, horizontal resoultion is 360 deg.
-	var vertical_resolution:float # LIDAR vertical resolution in radian
-	var horizontal_resolution:float # LIDAR horizontal resolution in radian
-	var range_distance:float
+## Sensor configurations
+#enum SENSOR_TYPE {RGB_CAMERA, DEPTH_CAMERA, RANGE, LIDAR}
+#
+## Sensor dictionary structure
+#class Sensor:
+#	var item_id:int # itemlist control id
+#	var enable:bool
+#	var name:String # name of ros2 published topic
+#	var type:SENSOR_TYPE
+#	var location:Vector3
+#	var rotation:Basis
+#	var hz:float # publish hz
+#	# advanced setting
+#	var resoultion:Vector2i # CAMERA TYPE RESOLUTION
+#	var vertical_fov:float # LIDAR in radian, horizontal resoultion is 360 deg.
+#	var vertical_resolution:float # LIDAR vertical resolution in radian
+#	var horizontal_resolution:float # LIDAR horizontal resolution in radian
+#	var range_distance:float
