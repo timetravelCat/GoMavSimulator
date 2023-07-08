eefd973e3961a08a841e77ffaeb0554e5b49a218
timetraveler <timetraveler930@gmail.com>
2023-07-08 15:04:53 +0900

    Implement third person camera and free camera, and user pose control mode.

--- modules/camera/free_fly/FreeFlyCamera.gd
+++ modules/camera/free_fly/FreeFlyCamera.gd
@@ -1,11 +1,24 @@
-extends Node
+@icon("./FreeFlyCameraIcon.png")
+class_name FreeFlyCamera extends Camera3D
 
 @export var control:bool = true
+@export var follow:Node3D
 @export var speed:float = 5.0
 @export var drag_sensitivity:float = 0.05
 
+var _basis_offset:Basis = Basis.from_euler(Vector3(0.0,deg_to_rad(-90.0),0.0), EULER_ORDER_YXZ)
+
+func _init():
+	global_transform = Transform3D(_basis_offset, Vector3.ZERO)
+
+func reset():
+	if follow:
+		global_transform = Transform3D(follow.global_transform.basis*_basis_offset, follow.global_position)
+	else:
+		global_transform = Transform3D(_basis_offset, Vector3.ZERO)
+
 func _process(delta):
-	if not control or not get_parent():
+	if not control:
 		return
 	var direction = Vector3.ZERO
 	if Input.is_key_pressed(KEY_W): 	direction.z -= 1
@@ -15,22 +28,21 @@ func _process(delta):
 	if Input.is_key_pressed(KEY_SPACE): direction.y += 1
 	if Input.is_key_pressed(KEY_C): 	direction.y -= 1
 	direction = direction.normalized()
-	get_parent().global_position += get_parent().global_transform.basis*direction*speed*delta
+	global_position += global_transform.basis*direction*speed*delta
 
 	var angle = 0.0
 	if Input.is_key_pressed(KEY_Q):	angle += 1.0
 	if Input.is_key_pressed(KEY_E):	angle -= 1.0
-	get_parent().global_transform.basis = get_parent().global_transform.basis * Basis(Vector3(0.0, 0.0, 1.0), angle*drag_sensitivity)
+	global_transform.basis = global_transform.basis * Basis(Vector3(0.0, 0.0, 1.0), angle*drag_sensitivity)
 
 func _input(event):
-	if not control or not get_parent():
+	if not control:
 		return
-
 	if event is InputEventMouseMotion:
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 			# Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 			var axis = event.relative.normalized()
 			if axis.length_squared() > 0.25:
-				get_parent().global_transform.basis = get_parent().global_transform.basis * Basis(Vector3(-axis.y, -axis.x, 0.0), drag_sensitivity)
+				global_transform.basis = global_transform.basis * Basis(Vector3(-axis.y, -axis.x, 0.0), drag_sensitivity)
 #	else:
 #		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
---
+++ modules/camera/third_person/ThirdPersonCamera.gd
@@ -0,0 +1,45 @@
+@icon("./ThirdPersonCameraIcon.svg")
+class_name ThirdPersonCamera extends Camera3D
+
+@export var control:bool = true
+@export var follow:Node3D
+@export var distance:float = 10.0
+@export var offset:Vector2 
+@export_range(-180.0,180.0,5.0,"suffix:°") var azimuth:float
+@export_range(-90.0,90.0,5.0,"suffix:°") var elevation:float
+@export_range(-90.0,90.0,5.0,"suffix:°") var tilt:float
+
+@export var drag_sensitivity:float = 0.1
+@export var zoom_sensitivity:float = 0.02
+
+@warning_ignore("unused_parameter")
+func _process(delta):
+	if not follow:
+		return
+	global_transform = Transform3D(
+		Basis.from_euler(Vector3(deg_to_rad(-elevation + tilt),deg_to_rad(-90.0 + azimuth),0.0), EULER_ORDER_YXZ),
+		follow.global_position + Basis.from_euler(Vector3(0.0, deg_to_rad(azimuth), deg_to_rad(-elevation)), EULER_ORDER_YXZ)*Vector3(-distance, offset.x, offset.y)
+	)
+
+func _input(event):
+	if not control:
+		return
+	if event is InputEventMouseMotion:
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+#			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			azimuth -= drag_sensitivity*event.relative.x
+			tilt -= drag_sensitivity*event.relative.y
+		elif Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
+#			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			azimuth -= drag_sensitivity*event.relative.x
+			elevation += drag_sensitivity*event.relative.y
+	else:
+#		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		if event is InputEventMouseButton:
+			if event.pressed:
+				match event.button_index:
+					MOUSE_BUTTON_WHEEL_DOWN:
+						distance += exp(distance*zoom_sensitivity)
+					MOUSE_BUTTON_WHEEL_UP:
+						distance -= exp(distance*zoom_sensitivity)
+				distance = clampf(distance, 0.0, 1000)
