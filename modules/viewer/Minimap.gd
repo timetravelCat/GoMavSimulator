098382fd207d31eb2288515153c29587fdc44d00
timetraveler <timetraveler930@gmail.com>
2023-07-13 09:33:53 +0900

    refactoring minimap scene (still has bug)

--- modules/viewer/Minimap.gd
+++ modules/viewer/Minimap.gd
@@ -1,42 +1,45 @@
 extends TextureRect
 
-@onready var camera:Camera3D = $SubViewport/Camera3D
-@onready var subViewport:SubViewport = $SubViewport
-@export var height:float = 20.0
-@export_range(0.0, 1.0, 0.01) var ratio:float = 0.5:
-	set(_ratio):
-		ratio = _ratio
-		resize()
-	
-@export_range(0.0, 1.0, 0.01) var alpha:float = 0.3:
-	set(_alpha):
-		alpha = _alpha
-		material.set_shader_parameter("alpha", _alpha)
-
-var _map_width_ratio:float = 1.0
-var _map_height_ratio:float = 1.0
+@export var height:float = 200.0 # TODO set camera hight depends on the vehicle's hieght
+@export_range(0.0, 1.0, 0.01) var ratio:float = 0.5: set = _ratio_initialize 
+@export_range(0.0, 1.0, 0.01) var alpha:float = 0.3: set = _alpha_initialize
 
 func _ready():
 	subViewport.world_3d = get_parent().get_window().world_3d
-	var district_volume = GeneralSettings._district_bb - GeneralSettings._district_aa
-	camera.size = maxf(district_volume.x, district_volume.z)/2.0
-	camera.global_position = Vector3(0.0, district_volume.y + height,0.0)
+	get_window().size_changed.connect(_on_window_resized)
+	_initialize_minimap()
+	GeneralSettings.district_changed.connect(_initialize_minimap)
 	
-	var _map_ratio = district_volume.z / district_volume.x
-	if _map_ratio > 1.0: # hight is longer than width
-		_map_width_ratio = 1.0/_map_ratio
-	else: # width is longer then height
-		_map_height_ratio = _map_ratio
-	
-	get_window().size_changed.connect(resize)
-	resize()
+func _initialize_minimap():
+	_ratio_initialize(ratio)
+	_alpha_initialize(alpha)
 
 #func _on_gui_input(event):
-#	# TODO implement zoom in / out
+#	TODO implement zoom in / out
 #	pass # Replace with function body.
 
-func resize():
-	var y_size:float = ratio*get_window().size.y
+func _on_window_resized():
+	_initialize_minimap()
+
+func _ratio_initialize(_ratio):
+	if not camera or not subViewport:
+		return
+	
+	ratio = _ratio
+	var district_size = GeneralSettings.district_AABB.size
+	camera.size = district_size.z/2.0
+	camera.global_position = GeneralSettings.current_district.global_position \
+							+ GeneralSettings.district_AABB.get_center() + \
+							Vector3(0.0, height, 0.0)
+	var _aspect_ratio = district_size.x / district_size.z
+	var _height_size:float = ratio*get_window().size.y
 	@warning_ignore("narrowing_conversion")
-	subViewport.size = Vector2i(_map_width_ratio*y_size, _map_height_ratio*y_size)
+	subViewport.size = Vector2i(_height_size/_aspect_ratio, _height_size)
 	size = subViewport.size
+
+func _alpha_initialize(_alpha):
+	alpha = _alpha
+	material.set_shader_parameter("alpha", _alpha)
+
+@onready var camera:Camera3D = $SubViewport/Camera3D
+@onready var subViewport:SubViewport = $SubViewport
