c521ceda09e15bf134bc6ed0b2454ed5b40a1d56
timetraveler <timetraveler930@gmail.com>
2023-07-13 12:52:50 +0900

    refactoring MavlinkSettings Scene.

---
+++ modules/settings/mavlink/MavlinkSettings.gd
@@ -0,0 +1,119 @@
+extends Node
+
+@export var default_settings:Dictionary = {
+	"open_on_connected" : true,
+	"close_on_disconnected" : true,
+	"system_id": 245,
+	"component_id": 190,
+	"connection_list": 
+		{
+			"udp://:14550" : false, 
+			"serial://COM3:57600" : true
+		},
+}
+
+@export_category("Console")
+@export var open_on_connected:bool
+@export var close_on_disconnected:bool
+func open_mavlink_shell():
+	_open_mavlink_shell()
+func close_mavlink_shell():
+	_close_mavlink_shell()
+
+@export_category("MAVSDK")
+@export var system_id:int = GoMAVSDKServer.get_system_id(): set = _change_system_id
+@export var component_id:int = GoMAVSDKServer.get_component_id(): set = _change_component_id
+@export var connection_list:Dictionary
+func add_or_enable_connection(address:String, enable:bool):
+	_add_or_enable_connection(address, enable)
+func remove_connection(address:String):
+	_remove_connection(address)
+
+# Not implemented joystick feature yet.
+#@export_category("JoyStick")
+#@export var joystick:bool
+#@export var virtual_joystick:bool
+
+func _ready():
+	_change_system_id(system_id)
+	_change_component_id(component_id)
+	GoMAVSDKServer.connect("shell_received", _on_shell_received)
+	GoMAVSDKServer.connect("system_discovered", _on_system_discovered)
+	GoMAVSDKServer.start_discovery() # start mavsdk discovery 
+
+# Console Methods
+@onready var console_timer = $ConsoleTimer
+@onready var console_container = $ConsoleContainer
+var console_scene:PackedScene = preload("res://modules/console/Console.tscn")
+func _on_console_timer_timeout():
+	for window in console_container.get_children():
+		if GoMAVSDKServer.is_system_connected(window.name.to_int()):
+			window.notify(0)
+		else:
+			window.notify(2)
+			if close_on_disconnected:
+				window.hide()
+func _open_mavlink_shell():
+	for window in console_container.get_children():
+		window.show()	
+	for sys_id in GoMAVSDKServer.get_discovered():
+		_create_new_mavlink_console(sys_id)
+func _close_mavlink_shell():
+	for window in console_container.get_children():
+		window.hide()	
+func _create_new_mavlink_console(sys_id:int)->Window:
+	if console_container.has_node(str(sys_id)):
+		return null
+	var window = console_scene.instantiate()
+	window.name = str(sys_id)
+	window.title = "SYSTEM ID : " + str(sys_id)
+	window.connect("text_submitted_with_sysid", GoMAVSDKServer.send_shell)
+	console_container.call_deferred("add_child", window)
+	return window
+	
+# MAVSDK
+@onready var autoconnect_timer = $AutoconnectTimer
+func _change_system_id(sys_id:int):
+	system_id = sys_id
+	GoMAVSDKServer.set_system_id(sys_id)
+func _change_component_id(comp_id:int):
+	component_id = comp_id
+	GoMAVSDKServer.set_component_id(comp_id)
+func _on_autoconnect_timer_timeout(): # try to reconnect until connection_list is all registered to MAVSDK server.
+	var restart:bool = false
+	for address in connection_list:
+		if connection_list[address]:
+			if GoMAVSDKServer.add_connection(address) != GoMAVSDKServer.CONNECTION_SUCCESS:
+				restart = true
+	if restart:
+		autoconnect_timer.start()
+func _on_shell_received(sys_id:int, msg:String):
+	if console_container.has_node(str(sys_id)):
+		console_container.get_node(str(sys_id)).append_contents(msg)
+func _on_system_discovered(sys_id:int):
+	var window = _create_new_mavlink_console(sys_id)
+	if !open_on_connected:
+		window.hide()
+func _restart_server():
+	GoMAVSDKServer.initialize(true) 
+	GoMAVSDKServer.start_discovery()
+	autoconnect_timer.start()
+func _add_or_enable_connection(address:String, enable:bool):
+	if !connection_list.has(address): # new connection
+		connection_list.merge({address:enable})
+		if enable:
+			autoconnect_timer.start()
+	else: # exist connection
+		if connection_list[address] != enable:
+			connection_list[address] = enable
+			if enable:
+				autoconnect_timer.start()
+			else:
+				_restart_server()
+func _remove_connection(address:String):
+	if !connection_list.has(address):
+		return
+	var enabled = connection_list[address]
+	connection_list.erase(address)
+	if enabled:
+		_restart_server()
--- modules/settings/mavlink/mavlink.gd
+++ modules/settings/mavlink/mavlink.gd
@@ -11,82 +11,71 @@ extends Control
 @export var serial:OptionButton
 
 func reset_to_default():
-	console_connect.select(0)
-	console_connect.item_selected.emit(0)
+	pass
+#	console_connect.select(0)
+#	console_connect.item_selected.emit(0)
+#
+#	console_disconnect.select(0)
+#	console_disconnect.item_selected.emit(0)
+#
+#	joystick_control.select(1)
+#	joystick_control.item_selected.emit(1)
+#
+#	virtual_joystick.select(1)
+#	virtual_joystick.item_selected.emit(1)
+#
+#	system_id.text = str(MavlinkSettings.DEFAULT_SYS_ID)
+#	system_id.text_submitted.emit(system_id.text)
+#
+#	component_id.text = str(MavlinkSettings.DEFAULT_COMP_ID)
+#	component_id.text_submitted.emit(component_id.text)
+#
+#	# remove all items in list
+#	while autoconnect_list.item_count > 0:
+#		autoconnect_list.select(0)
+#		_on_autoconnect_remove_pressed()
 	
-	console_disconnect.select(0)
-	console_disconnect.item_selected.emit(0)
-	
-	joystick_control.select(1)
-	joystick_control.item_selected.emit(1)
-	
-	virtual_joystick.select(1)
-	virtual_joystick.item_selected.emit(1)
-	
-	system_id.text = str(MavlinkSettings.DEFAULT_SYS_ID)
-	system_id.text_submitted.emit(system_id.text)
-	
-	component_id.text = str(MavlinkSettings.DEFAULT_COMP_ID)
-	component_id.text_submitted.emit(component_id.text)
-	
-	# remove all items in list
-	while autoconnect_list.item_count > 0:
-		autoconnect_list.select(0)
-		_on_autoconnect_remove_pressed()
-	
-	# Add default item 
-	for key in MavlinkSettings.DEFAULT_AUTOCONNECT_LIST:
-		var autoConnect:MavlinkSettings.AutoConnect = MavlinkSettings.AutoConnect.new()
-		autoConnect.index = autoconnect_list.add_item(key)
-		autoConnect.enabled = MavlinkSettings.DEFAULT_AUTOCONNECT_LIST[key]
-		MavlinkSettings.AutoConnectList.merge({key:autoConnect})
-		if !autoConnect.enabled:
-			autoconnect_list.set_item_custom_fg_color(autoConnect.index, Color(1.0, 1.0, 1.0, 0.2))
-	MavlinkSettings.auto_connect_list_changed.emit(true)
+# Add default item Required to Save
+
+#	for key in MavlinkSettings.default_connection_list:
+#		var autoConnect:MavlinkSettings.AutoConnect = MavlinkSettings.AutoConnect.new()
+#		autoConnect.index = autoconnect_list.add_item(key)
+#		autoConnect.enabled = MavlinkSettings.default_connection_list[key]
+#		MavlinkSettings.connection_list.merge({key:autoConnect})
+#		if !autoConnect.enabled:
+#			autoconnect_list.set_item_custom_fg_color(autoConnect.index, Color(1.0, 1.0, 1.0, 0.2))
+#	MavlinkSettings.auto_connect_list_changed.emit(true)
 
 func _on_console_connected_item_selected(index):
 	match index:
 		0:
-			MavlinkSettings.open_console_when_connected = true
+			MavlinkSettings.open_on_connected = true
 		1:
-			MavlinkSettings.open_console_when_connected = false
+			MavlinkSettings.open_on_connected = false
 	pass
 
 func _on_console_disconnected_item_selected(index):
 	match index:
 		0:
-			MavlinkSettings.close_console_when_disconnected = true
+			MavlinkSettings.close_on_disconnected = true
 		1:
-			MavlinkSettings.close_console_when_disconnected = false
+			MavlinkSettings.close_on_disconnected = false
 	pass
 
 func _on_open_console_pressed():
-	MavlinkSettings.open_mavlink_shell.emit()
-	pass
-
+	MavlinkSettings.open_mavlink_shell();
 func _on_close_console_pressed():
-	MavlinkSettings.close_mavlink_shell.emit()
-	pass
+	MavlinkSettings.close_mavlink_shell()
 
+# Not Implemented yet.
 func _on_joystick_control_item_selected(index):
-	match index:
-		0:
-			MavlinkSettings.joystick_enabled.emit(true)
-		1:
-			MavlinkSettings.joystick_enabled.emit(false)
 	pass
-
 func _on_virtual_joystick_item_selected(index):
-	match index:
-		0:
-			MavlinkSettings.virutal_joystick_enabled.emit(true)
-		1:
-			MavlinkSettings.virutal_joystick_enabled.emit(false)
 	pass
 
 func _on_system_id_text_submitted(new_text:String):
 	if new_text.is_valid_int():
-		MavlinkSettings.system_id_changed.emit(new_text.to_int())
+		MavlinkSettings.system_id = new_text.to_int()
 	else:
 		Notification.notify(" Unvalid system id ", Notification.NOTICE_TYPE.ALERT)
 		system_id.clear()
@@ -96,24 +85,19 @@ func _on_component_id_text_submitted(new_text:String):
 		Notification.notify(" Unvalid comp id ", Notification.NOTICE_TYPE.ALERT)
 		component_id.clear()
 		return
-		
-	MavlinkSettings.component_id_changed.emit(new_text.to_int())
+	MavlinkSettings.component_id = new_text.to_int()
 
 func _on_autoconnect_add_pressed():
 	if autoconnect.text.is_empty():
 		Notification.notify(" Empty Connection Address ", Notification.NOTICE_TYPE.ALERT)
 		return 
 		
-	if MavlinkSettings.AutoConnectList.has(autoconnect.text):
+	if MavlinkSettings.connection_list.has(autoconnect.text):
 		Notification.notify(" Connection Address Already Exist ", Notification.NOTICE_TYPE.WARNING)
 		return 
 	# TODO Check about string by using REGEX?
-	
-	var autoConnect:MavlinkSettings.AutoConnect = MavlinkSettings.AutoConnect.new()
-	autoConnect.index = autoconnect_list.add_item(autoconnect.text)
-	autoConnect.enabled = true
-	MavlinkSettings.AutoConnectList.merge({autoconnect.text:autoConnect})
-	MavlinkSettings.auto_connect_list_changed.emit(false)
+	autoconnect_list.add_item(autoconnect.text)
+	MavlinkSettings.add_or_enable_connection(autoconnect.text, true)
 
 func _on_autoconnect_remove_pressed():
 	if !autoconnect_list.is_anything_selected():
@@ -121,9 +105,8 @@ func _on_autoconnect_remove_pressed():
 		return
 	
 	var selected_item:int = autoconnect_list.get_selected_items()[0]
-	MavlinkSettings.AutoConnectList.erase(autoconnect_list.get_item_text(selected_item))
+	MavlinkSettings.remove_connection(autoconnect_list.get_item_text(selected_item))
 	autoconnect_list.remove_item(selected_item)
-	MavlinkSettings.auto_connect_list_changed.emit(true)
 
 func _on_autoconnect_enable_pressed():
 	if !autoconnect_list.is_anything_selected():
@@ -131,10 +114,9 @@ func _on_autoconnect_enable_pressed():
 		return
 	
 	var selected_item:int = autoconnect_list.get_selected_items()[0]
-	MavlinkSettings.AutoConnectList[autoconnect_list.get_item_text(selected_item)].enabled = true
+	MavlinkSettings.add_or_enable_connection(autoconnect_list.get_item_text(selected_item), true)
 	autoconnect_list.set_item_custom_fg_color(selected_item, Color(1.0, 1.0, 1.0, 1.0))
 	autoconnect_list.set_item_tooltip(selected_item, "enabled")
-	MavlinkSettings.auto_connect_list_changed.emit(false)
 			
 func _on_autoconnect_disable_pressed():
 	if !autoconnect_list.is_anything_selected():
@@ -142,10 +124,9 @@ func _on_autoconnect_disable_pressed():
 		return
 	
 	var selected_item:int = autoconnect_list.get_selected_items()[0]
-	MavlinkSettings.AutoConnectList[autoconnect_list.get_item_text(selected_item)].enabled = false
+	MavlinkSettings.add_or_enable_connection(autoconnect_list.get_item_text(selected_item), false)
 	autoconnect_list.set_item_custom_fg_color(selected_item, Color(1.0, 1.0, 1.0, 0.2))
 	autoconnect_list.set_item_tooltip(selected_item, "disabled")
-	MavlinkSettings.auto_connect_list_changed.emit(true)
 
 var serial_item:Array
 
